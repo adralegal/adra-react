382666fe4b60e51fdd95aa3b915b0fae0eb54121
Priyank Singh <priyanksingh8@gmail.com>
2020-07-21 19:08:22 +0530

    Optimized for mobile for now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect } from "react"
 
-function App() {
+import Blog from "./components/blog/blog"
+import BlogRow from "./components/blogs-row/blogs-row"
+import "./index.css";
+import Layout from "./components/layout"
+
+const blogs = [
+  <Blog key={0} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+  <Blog key={1} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+  <Blog key={2} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+  <Blog key={3} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+  <Blog key={4} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+  <Blog key={5} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+  <Blog key={6} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+  <Blog key={7} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+  <Blog key={8} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+]
+
+
+
+//TODO use navbar and sidebar as component of layout
+const App = (props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Layout mobile={props.isMobile}>
+        <BlogRow header="Recently Added" content={blogs} />
+      </Layout>
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { Route, Router, Switch } from "react-router-dom";
+
+import history from "./utils/history";
+import "./fonts/fonts.css";
 import './index.css';
 import App from './App';
+import ProtectedRoute from "./components/hoc/ProtectedRoute";
+import UploadPage from "./components/uploadPage/upload.js";
 import * as serviceWorker from './serviceWorker';
+
+
+const userReducer = require("./store/userReducer");
+const { FirebaseContext, default: Firebase } = require("./components/firebase");
+const store = configureStore({
+  reducer: userReducer
+});
+
+let isMobile = false;
+if(window.innerWidth <= 500) {
+  isMobile = true
+}
+else isMobile = false;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router history={history}>
+      <Provider store={store}>
+        <FirebaseContext.Provider value={new Firebase()}>
+          <Switch>
+            <ProtectedRoute exact path={"/upload"} component={UploadPage} />
+            <Route path={"/"} render={() => <App isMobile={isMobile}/>} />
+          </Switch>
+        </FirebaseContext.Provider>
+      </Provider>
+    </Router>
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
