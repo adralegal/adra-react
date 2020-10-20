6125775e330059cbb54f1c1bd39841ce1e862cf0
Priyank Singh <priyanksingh8@gmail.com>
2020-10-20 16:47:47 +0530

    firebase: added fetching of documents in front screen

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React, { useContext, useEffect } from "react"
-
-import Blog from "./components/blog/blog"
-import BlogRow from "./components/blogs-row/blogs-row"
+import React, { useContext, useEffect, useState } from "react";
+import Blog from "./components/blog/blog";
+import BlogRow from "./components/blogs-row/blogs-row";
 import "./index.css";
 import Layout from "./components/layout"
 import PageIndicator from "./components/ui/page-indicator/pageIndicator";
 
-const blogs = [
-  <Blog key={0} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-  <Blog key={1} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-  <Blog key={2} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-  <Blog key={3} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-  <Blog key={4} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-  <Blog key={5} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-  <Blog key={6} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-  <Blog key={7} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-  <Blog key={8} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
-]
+import { FirebaseContext } from "./components/firebase";
+
+
+
+// const blogs = [
+//   <Blog key={0} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+//   <Blog key={1} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+//   <Blog key={2} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+//   <Blog key={3} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+//   <Blog key={4} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+//   <Blog key={5} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+//   <Blog key={6} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+//   <Blog key={7} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+//   <Blog key={8} title="Dummy Title" content={`The content of your new blog will be the bait that attracts your readers. In this guide, we will cover what content you need to write when you start a blog and the best practices you should follow.`} />,
+// ]
+
+
 
 
 
 //TODO use navbar and sidebar as component of layout
 const App = (props) => {
+  const [blogs, setBlogs] = useState(0);
+  const fb = useContext(FirebaseContext);
+  const blogRef = fb.db.collection('blogs');
+  useEffect(() => {
+    (async () => {
+      const blogSnapshot = await blogRef.orderBy('createdAt').limit(10).get();
+      const blogComps = [];
+      blogSnapshot.forEach(doc => {
+        blogComps.push(<Blog key={doc.id} content={doc.data().content} title={doc.data().title} />);
+      });
+      setBlogs(blogComps);
+    })();
+  },[]);
   console.log(props);
   return (
     <>
